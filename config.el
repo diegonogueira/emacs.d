ea0cfde156c27a26140adf819b64b3747370b3c4
Diego Nogueira <diegoalvareznogueira@gmail.com>
2019-08-07 13:16:43 -0300

    Inicial commit

---
+++ config.el
@@ -0,0 +1,215 @@
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(unless (package-installed-p 'zenburn-theme)
+  (package-refresh-contents)
+  (package-install 'zenburn-theme))
+
+(unless (package-installed-p 'nova-theme)
+  (package-refresh-contents)
+  (package-install 'nova-theme))
+
+(line-number-mode 1)
+(column-number-mode 1)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-subword-mode 1)
+
+(setq display-time-24hr-format t)
+(display-time-mode 1)
+
+(setq electric-pair-pairs '(
+			   (?\( . ?\))
+			   (?\[ . ?\])
+			   (?\{ . ?\})
+			   ))
+(electric-pair-mode t)
+
+(setq make-backup-files nil) 
+(setq auto-save-default nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq ring-bell-function 'ignore)
+
+(when window-system (global-prettify-symbols-mode t))
+
+(when window-system (global-hl-line-mode t))
+
+(setq inhibit-startup-message t)
+(setq scroll-conservatively 100)
+
+(defvar my-term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+(use-package hungry-delete
+  :ensure t
+  :config (global-hungry-delete-mode))
+
+(use-package sudo-edit
+  :ensure t
+  :bind ("s-e" . sudo-edit))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents . 10)))
+  (setq dashboard-banner-logo-title "Dashboard"))
+
+(use-package spaceline
+  :ensure t
+  :config
+  (require 'spaceline-config)
+  (setq powerline-default-separator (quote arrow))
+  (spaceline-spacemacs-theme))
+
+(use-package diminish
+  :ensure t
+  :init
+  (diminish 'hungry-delete-mode)
+  (diminish 'beacon-mode)
+  (diminish 'subword-mode)
+  (diminish 'rainbow-mode)
+  (diminish 'which-key-mode))
+
+(use-package dmenu
+  :ensure t
+  :bind
+  ("s-SPC" . 'dmenu))
+
+(use-package symon
+  :ensure t
+  :bind
+  ("s-h" . symon-mode))
+
+(use-package beacon
+  :ensure t
+  :init
+  (beacon-mode 1))
+
+(ido-mode 1)
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1))
+
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind
+  ("M-x" . smex))
+
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+
+(defun kill-all-buffers()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+(global-set-key (kbd "C-M-k") 'kill-all-buffers)
+
+(defun kill-curr-buffer ()
+  (interactive)
+  (kill-buffer (current-buffer)))
+(global-set-key (kbd "C-x k") 'kill-curr-buffer)
+
+(global-set-key (kbd "C-x b") 'ibuffer)
+
+(setq ibuffer-expert t)
+
+(use-package avy
+  :ensure t
+  :bind
+  ("M-s" . avy-goto-char))
+
+(defun kill-whole-word ()
+  (interactive)
+  (backward-word)
+  (kill-word 1))
+(global-set-key (kbd "C-c w w") 'kill-whole-word)
+
+(defun copy-whole-line ()
+  (interactive)
+  (save-excursion
+    (kill-new
+     (buffer-substring
+      (point-at-bol)
+      (point-at-eol)))))
+(global-set-key (kbd "C-c w l") 'copy-whole-line)
+
+(setq org-src-window-setup 'current-window)
+(add-to-list 'org-structure-template-alist
+	     '("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
+
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
+(global-set-key (kbd "C-c e") 'config-visit)
+
+(defun config-reload ()
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(global-set-key (kbd "C-c r") 'config-reload)
+
+(use-package rainbow-mode
+  :ensure t
+  :init (rainbow-mode 1))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (rainbow-delimiters-mode 1))
+
+(use-package switch-window
+  :ensure t
+  :config
+  (setq switch-window-input-style 'minibuffer)
+  (setq swtich-window-increase 4)
+  (setq switch-window-threshold 2)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts
+	'("a" "s" "d" "f" "h" "j" "k" "l"))
+  :bind
+  ([remap other-window] . switch-window))
+
+(defun split-and-follow-horizontally()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+
+(defun split-and-follow-vertically()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
---
+++ init.el
@@ -0,0 +1,86 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(ansi-term-color-vector
+   [unspecified "#2e2e2e" "#bc8383" "#7f9f7f" "#d0bf8f" "#6ca0a3" "#dc8cc3" "#8cd0d3" "#b6b6b6"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("8150ded55351553f9d143c58338ebbc582611adc8a51946ca467bd6fa35a1075" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "28335ebd9ad0ee69c9ce4157eb621bb93b1bc31d054b48952cb9887a4d863fad" "c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" default)))
+ '(fci-rule-color "#383838")
+ '(frame-background-mode (quote dark))
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f"))))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window beacon rainbow-mode avy org-bullets smex ido-vertical-mode grayscale-theme sgrayscale-theme spacemacs-theme nova-theme zenburn-theme use-package)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#3C4C55")
+ '(vc-annotate-color-map
+   (\`
+    ((20 \, "#DF8C8C")
+     (40 \, "#e3af97978d26")
+     (60 \, "#e780a2a28dc0")
+     (80 \, "#eb50adac8e5a")
+     (100 \, "#ef21b8b88ef4")
+     (120 \, "#F2C38F")
+     (140 \, "#ee20c861905c")
+     (160 \, "#e94eccff912a")
+     (180 \, "#e47dd19d91f7")
+     (200 \, "#dfabd63b92c5")
+     (220 \, "#DADA93")
+     (240 \, "#d0d0d8719393")
+     (260 \, "#c6c6d6089393")
+     (280 \, "#bcbcd39f9393")
+     (300 \, "#b2b2d1369393")
+     (320 \, "#A8CE93")
+     (340 \, "#a13ac894a409")
+     (360 \, "#99ccc25bb480")
+     (380 \, "#925ebc21c4f7")
+     (400 \, "#8af0b5e8d56e")
+     (420 \, "#83AFE5")
+     (440 \, "#8821aa0fe517")
+     (460 \, "#8cbfa470e449")
+     (480 \, "#915d9ed1e37c")
+     (500 \, "#95fb9932e2ae")
+     (520 \, "#9A93E1"))))
+ '(vc-annotate-very-old-color "#7bae760fb4b4"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Monaco")))))
